a791417d37e46c95ca9458871a84b4b2a2d48318
Pinkey <rinzafariapinkey@gmail.com>
2025-09-28 10:13:03 +0600

    edit ---11

---
+++ 11.cpp
@@ -0,0 +1,46 @@
+//Linked list
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+   int data;
+   Node* next;
+};
+
+void solve(){
+     Node* head=NULL;
+     Node* temp=NULL;
+     Node* last=NULL;
+
+     int n;
+     cin>>n;
+     int v[n];
+     for(int i=0;i<n;i++){
+        cin>>v[i];
+     }
+     for(int i=0;i<n;i++){
+         temp=new Node();
+         temp->data=v[i];
+        // temp->next=NULL;
+
+         if(head==NULL){
+            head=temp;
+            last=temp;
+         }else{
+             last->next=temp;
+             last=temp;
+         }
+     }
+     Node* current=head;
+     cout<<"Linked list : ";
+     while(current!=NULL){
+        cout<<current->data<<" ";
+        current=current->next;
+     }
+     cout<<endl;
+}
+
+
+int main(){
+     solve();
+}
